37abd43d3dee63aa16d6d39a5d648ed7400cab8f
fedmag <maggiorefederico@outlook.com>
2023-05-26 20:44:27 +0200

    moved ui to process, fixed closing of the app while running

--- src/main.py
+++ src/main.py
@@ -4,22 +4,23 @@ from components.event_handlers.clock_handler import ClockEventHandler
 from sounds.speaker import Speaker
 from gsi.server import ServerManager
 from ui.GUI import run_gui 
-from queue import Queue
-import tkinter
+import queue
+import sys
+import time
 import logging
 
 logging.basicConfig(level=logging.INFO)
 logging.getLogger("comtypes").setLevel(logging.WARNING)
 log = logging.getLogger(__name__)
 
-def run():
+def run(thd):
     tracked_events = [
         CountDown(Event("Lotus and gold runes spawning in 15 seconds", 3*60)), 
         CountDown(Event("Wisdom rune spawning 15 seconds", 7*60)),
         CountDown(Event("Tormentor spawned", 20*60, recurring = False)) # a one time event as there is no way of knowing when it is killed
     ]
 
-    server = ServerManager(q = Queue())
+    server = ServerManager(q = queue.Queue())
     server.start()
 
     clock_handler = ClockEventHandler()
@@ -30,25 +31,30 @@ def run():
     speaker = Speaker()
     speaker.say("Counters are set!")
     
-    try:
-        while True: 
-            state: str = server.q.get()
+
+    while True: 
+        try:
+            state: str = server.q.get(block=False)
             [handler.handle_state(state) for handler in handlers]
             game_clock = clock_handler.get_game_clock()
             for tracked_event in tracked_events:
                 if tracked_event.handle_time(game_clock):
                     speaker.say(tracked_event.get_event_name())
 
-    except KeyboardInterrupt:
-        log.info("Interruption signal sent.")
-        server.stop()
-        speaker.stop()
-        log.info("Program stopped")
-        pass
+        except queue.Empty:
+            pass
+        except KeyboardInterrupt:
+            log.info("Starting shutdown procedure..")
+            time.sleep(2)
+            log.info("Interruption signal sent.")
+            server.stop()
+            speaker.stop()
+            thd.kill()
+            log.info("Program stopped")
+            sys.exit(1)
 
 if __name__ == "__main__":
-    import threading
-    thd = threading.Thread(target=run_gui)   # gui thread
-    thd.daemon = True  # background thread will exit if main thread exits
+    import multiprocessing as mp
+    thd = mp.Process(target=run_gui)   # gui process
     thd.start()  # start tk loop
-    run()
+    run(thd)
