6d024573548fdb8d0763ad9b4d07fdb42922bbbd
fedmag <maggiorefederico@outlook.com>
2023-05-26 20:43:31 +0200

    moved to data class

--- src/components/event.py
+++ src/components/event.py
@@ -1,15 +1,7 @@
-import pyttsx3
+from dataclasses import dataclass
 
+@dataclass
 class Event:
-
-    def __init__(self, name: str, amount: float, recurring: bool = True) -> None:
-        self.name = name
-        self.amount = amount
-        self.recurring = recurring
-        self.tts = pyttsx3.init()
-        self.tts.runAndWait()
-
-
-    def alarm(self):
-        print(f"emitting sound for {self.name}")
-        self.tts.say(self.name)
+    name: str
+    amount: int
+    recurring: bool = True
