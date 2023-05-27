55235910b7486bca0a89dc47146216845ef047e2
fedmag <maggiorefederico@outlook.com>
2023-05-28 00:07:37 +0200

    adapted classes for settings file

--- src/components/countdown.py
+++ src/components/countdown.py
@@ -9,15 +9,14 @@ class CountDown:
     def __init__(self, event: Event) -> None:
         log.info(f"Setting up timer for {event.name}")
         self.__event = event
-        self.__OFFSET = 15
         self.__last_played: int = 0
     
     def get_event_name(self):
-        return self.__event.name
+        return f"{self.__event.sentence} in {self.__event.offset} seconds."
     
     def handle_time(self, game_clock: int) -> bool:
         if game_clock < 1 or (not self.__event.recurring and self.__last_played != 0): return False # for single time events (like tormentor)
-        if (game_clock + self.__OFFSET) % self.__event.amount == 0 and game_clock != self.__last_played:
+        if (game_clock + self.__event.offset) % self.__event.amount == 0 and game_clock != self.__last_played:
             self.__last_played = game_clock
             return True
         return False
--- src/components/event.py
+++ src/components/event.py
@@ -4,4 +4,10 @@ from dataclasses import dataclass
 class Event:
     name: str
     amount: int
+    sentence: str
+    offset: int = 15
     recurring: bool = True
+    
+    @classmethod
+    def create_from_dict(cls, **kwarg):
+        return Event(**kwarg)
